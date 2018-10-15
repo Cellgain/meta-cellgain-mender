e230e5545eac99731413078a2f3dcbb04c87205e
Jose Moffa <moffa3@gmail.com>
2018-10-15 02:27:29 -0300

    fix scripts

--- recipes-nms/nms-server/files/start-ap.sh
+++ recipes-nms/nms-server/files/start-ap.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-systemctl enable wpa_supplicant-ap@wlan0
-
 systemctl start wpa_supplicant-ap@wlan0
 
-sleep 5
+sleep 15
 
 systemctl restart systemd-networkd
 
-systemctl start wpa_supplicant-ap@wlan0
+sleep 15
 
-sleep 5
-
-systemctl restart systemd-networkd
